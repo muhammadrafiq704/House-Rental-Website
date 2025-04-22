f21d4e5d3ba0b625ad1cbc101676ecfb6b8247b1
Muhammad Rafiq <sameerbalti704@gmail.com>
2025-04-22 19:53:03 +0500

    feat: added auth model, controller, routes

---
+++ Backend/Auth/Register.js
@@ -0,0 +1,39 @@
+import bcrypt from "bcryptjs";
+import Register from "../Models/AuthModels/RegisterModel.js";
+
+export const registerUser = async (req, res) => {
+	try {
+		const { username, email, password, confirm_password } = req.body;
+
+		if (!username || !email || !password || !confirm_password) {
+			return res.status(400).json({ message: "All fields are required" });
+		}
+
+		const existingUser = await Register.findOne({ email });
+		if (existingUser) {
+			return res.status(400).json({ message: "Email already in use" });
+		}
+
+		if (password !== confirm_password) {
+			return res.status(400).json({ message: "Passwords do not match" });
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const newUser = new Register({
+			username,
+			email,
+			password: hashedPassword,
+		});
+
+		await newUser.save();
+
+		res.status(201).json({
+			message: "User registered successfully",
+		});
+	} catch (error) {
+		console.error("Registration error:", error);
+		res.status(500).json({ message: "Server error" });
+	}
+};
--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import AuthRoutes from "./Routes/AuthRoutes.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -16,7 +17,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 // Routes
-// app.use('/api/users', );
+app.use("/api", AuthRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;

a8f6691d843353443d141113efeb0035f09062ee
Muhammad Rafiq <sameerbalti704@gmail.com>
2025-05-10 01:23:31 +0500

    feat: added single property controller, routes

--- Backend/Routes/PropertyRoutes.js
+++ Backend/Routes/PropertyRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createProperty } from "../Controllers/Property/CreatePropertyController.js";
 import { getProperties } from "../Controllers/Property/GetProperty.js";
 import { getPropertyTypes } from "../Controllers/Property/PropertyCategory.js";
+import { getSingleProperty } from "../Controllers/Property/SignleProperty.js";
 import upload from "../config/multer.js";
 
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 router.post("/", upload.array("file", 5), createProperty);
 router.get("/", getProperties);
 router.get("/:type", getPropertyTypes);
+router.get("/property/:id", getSingleProperty);
 
 export default router;
